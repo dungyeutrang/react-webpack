3318e6e6d76e4f5622d3e974a471936210b8dd6b
dungpv <dungpv@rikkeisoft.com>
2016-10-26 17:36:20 +0700

    Begin app

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+var config = {
+    entry: './main.js',
+
+    output: {
+        path: './',
+        filename: 'index.js',
+    },
+
+    devServer: {
+        inline: true,
+        port: 8080
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: JSON.stringify("production")
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+            },
+            output: {
+                comments: false,
+            },
+        }),
+    ]
+}
+
+module.exports = config;
