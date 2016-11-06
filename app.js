3ee374bbb752af9123977e4fe4958b9e473b52db
dungpv <dungpv@rikkeisoft.com>
2016-11-06 22:47:45 +0700

    build layout

--- app.js
+++ app.js
@@ -2,17 +2,20 @@
     var app = express()
     var multer  = require('multer')
     var upload = multer();
-
+    var engine = require('ejs-locals')
+    var path = require('path');
     var bodyParser = require('body-parser');
     var multer  = require('multer');
 
+    app.engine('ejs',engine);
     app.use(express.static('public'));
     app.use(bodyParser.urlencoded({ extended: false }));
     app.set('view engine', 'ejs');
+    app.set('views', path.join(__dirname, '/src/client/view'));
 
     // respond with "hello world" when a GET request is made to the homepage
     app.get('/', function (req, res) {
-        res.send('hello world')
+        res.render('home/index');
     })
 
     app.listen(3000, function () {
